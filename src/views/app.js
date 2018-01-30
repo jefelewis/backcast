8a7459e5b8180a16107bec2b9328ef35fb2dc4e7
Hack Reactor Student <student@hackreactor.com>
2018-01-30 10:56:21 -0800

    Titles/description working

--- src/views/app.js
+++ src/views/app.js
@@ -12,11 +12,25 @@ var AppView = Backbone.View.extend({
 
   render: function() {
     this.$el.html(this.template());
+
+    // Video List View
     new VideoListView({
       collection: this.videos,
       el: this.$('.list')
     }).render();
     console.log('render been called');
+
+    // Search View
+    new searchView({
+      collection: this.videos,
+      el: this.$('.list')
+    }).render();
+
+    // Video Player View
+    new VideoPlayerView({
+      collection: this.videos,
+      el: this.$('.list')
+    }).render();
   },
 
   template: templateURL('src/templates/app.html'),
